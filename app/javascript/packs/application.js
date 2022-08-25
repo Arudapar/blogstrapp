2227aec69eae5081f7d52e7b56317d02cb8f34ca
Arudapar <yuri.arudapar@yahoo.com>
2022-08-24 23:32:11 -0300

    Funcionalidade Do Javascript No Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,16 +4,22 @@
 // that code so it'll be compiled.
 //= require jquery
 //= require jquery_ujs
+require("@popperjs/core");
 
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
+import { Tooltip } from "bootstrap"
 import '../stylesheets/application'
 
-
+document.addEventListener("turbolinks:load", () => {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+})
 
 Rails.start()
 Turbolinks.start()
